cb0ed138361323734a5876aa2701e038d0f621f7
Erick Fellipe <efrc22br@gmail.com>
2026-04-02 18:09:57 -0300

    Inicializado estilização do formulario de cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegisterFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RegisterFormComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
