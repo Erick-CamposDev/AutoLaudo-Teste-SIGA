a1ade2e73e0c47c815e9108613c9306a4f498ba8
Erick Fellipe <efrc22br@gmail.com>
2026-04-03 17:33:54 -0300

    Finalizado estilização do formulario inicial

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ErrorMessagesComponent } from '../error-messages/error-messages.component';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css']
+  styleUrls: ['./register-form.component.css'],
 })
 export class RegisterFormComponent {
+  selectedType: string = '';
+  @ViewChild(ErrorMessagesComponent) errorMessages!: ErrorMessagesComponent;
 
+  showReport() {
+    this.errorMessages.validRadioCheck();
+  }
 }
