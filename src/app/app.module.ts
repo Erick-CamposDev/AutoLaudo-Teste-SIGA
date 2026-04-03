d6a911817bd3c15c0443f1ba9fe31c1b240cff41
Erick Fellipe <efrc22br@gmail.com>
2026-04-03 17:41:57 -0300

    Ajustes nos demais arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { ErrorMessagesComponent } from './components/error-messages/error-messages.component';
+import { MainReportComponent } from './components/main-report/main-report.component';
+import { ReportFormComponent } from './components/report-form/report-form.component';
+import { ItemChecklistComponent } from './components/item-checklist/item-checklist.component';
 
 @NgModule({
-  declarations: [AppComponent, RegisterFormComponent],
+  declarations: [AppComponent, RegisterFormComponent, ErrorMessagesComponent, MainReportComponent, ReportFormComponent, ItemChecklistComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
