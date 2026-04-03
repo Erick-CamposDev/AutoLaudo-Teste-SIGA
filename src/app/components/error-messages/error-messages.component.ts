19f86c2a0ed0a88aa48937dc14353b059b24140c
Erick Fellipe <efrc22br@gmail.com>
2026-04-03 15:02:42 -0300

    Adicionado componente para mensagens de erro

---
+++ src/app/components/error-messages/error-messages.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error-messages',
+  templateUrl: './error-messages.component.html',
+  styleUrls: ['./error-messages.component.css'],
+})
+export class ErrorMessagesComponent {
+  @Input() selectedType: string = '';
+  radioError: boolean = false;
+
+  validRadioCheck() {
+    if (!this.selectedType) {
+      this.radioError = true;
+    } else {
+      this.radioError = false;
+    }
+  }
+}
