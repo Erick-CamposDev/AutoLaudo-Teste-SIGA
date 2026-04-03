1515f31a238692352ea93612e209fb37137f4ebb
Erick Fellipe <efrc22br@gmail.com>
2026-04-03 17:40:39 -0300

    Adicionado componente de checklist

---
+++ src/app/components/item-checklist/item-checklist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-checklist',
+  templateUrl: './item-checklist.component.html',
+  styleUrls: ['./item-checklist.component.css']
+})
+export class ItemChecklistComponent {
+
+}
