e0940d68738412b92bc2ec75f07f12ea12a5023a
Erick Fellipe <efrc22br@gmail.com>
2026-04-04 13:19:20 -0300

    Criada e modificada componente de tabela

---
+++ src/app/components/table-checklist/table-checklist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-checklist',
+  templateUrl: './table-checklist.component.html',
+  styleUrls: ['./table-checklist.component.css']
+})
+export class TableChecklistComponent {
+
+}
