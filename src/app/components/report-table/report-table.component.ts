f0dd171bad3727f504670bc71efb66cd6b36eeac
Erick Fellipe <efrc22br@gmail.com>
2026-04-05 17:25:06 -0300

    Adicionado, testado e finalizado relatorio

--- src/app/components/report-table/report-table.component.ts
+++ src/app/components/report-table/report-table.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { VehicleInformation } from 'src/app/models/VehicleInfo';
 
 @Component({
   selector: 'app-report-table',
   templateUrl: './report-table.component.html',
-  styleUrls: ['./report-table.component.css']
+  styleUrls: ['./report-table.component.css'],
 })
 export class ReportTableComponent {
+  @Input() vehicleInfo: VehicleInformation[] = [];
+  @Input() vehicleRating: any;
+  @Input() formData: any;
+  leftItems: any[] = [];
+  rightItems: any[] = [];
 
+  ngOnChanges() {
+    const half = Math.ceil(this.vehicleInfo.length / 2);
+    this.leftItems = this.vehicleInfo.slice(0, half);
+    this.rightItems = this.vehicleInfo.slice(half);
+  }
 }
